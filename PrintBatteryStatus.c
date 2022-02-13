87053d54da8162f7fa26d258ac379b34240aff3c
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-02-13 23:10:21 +0530

    Warning limit inclusion

--- PrintBatteryStatus.c
+++ PrintBatteryStatus.c
@@ -3,7 +3,7 @@
 void printhigherlimitexceeded(){
     if((batterystatus[0]) & 1) printf("Temperature exceeds higher limit!\n");
     if((batterystatus[0]) & 2) printf("SOC exceeds higher limit!\n");
-    if((batterystatus[0]d) & 4) printf("Charge rate exceeds higher limit!\n");
+    if((batterystatus[0]) & 4) printf("Charge rate exceeds higher limit!\n");
     printf("%d %d %d %d", batterystatus[0], batterystatus[1], batterystatus[2], batterystatus[3]);
 }
 
@@ -14,15 +14,15 @@ void printlowerlimitexceeded(){
 }
 
 void printhigherlimitwarning(){
-    if((batterystatu[1]) & 1) printf("Warning! Temperature is about to exceed the higher limit!\n");
-    if((batterystatu[1]) & 2) printf("Warning! SOC is about to exceed the higher limit!\n");
-    if((batterystatu[1]) & 4) printf("Warning! Charge rate is about to exceed the higher limit!\n");
+    if((batterystatus[1]) & 1) printf("Warning! Temperature is about to exceed the higher limit!\n");
+    if((batterystatus[1]) & 2) printf("Warning! SOC is about to exceed the higher limit!\n");
+    if((batterystatus[1]) & 4) printf("Warning! Charge rate is about to exceed the higher limit!\n");
 }
 
 void printlowerlimitwarning(){
-    if((batterystatu[3]) & 1) printf("Warning! Temperature is about to breach the lower limit!\n");
-    if((batterystatu[3]) & 2)printf("Warning! SOC is about to breach the lower limit!\n");
-    if((batterystatu[3]) & 4) printf("Warning! Charge rate is about to breach the lower limit!\n");
+    if((batterystatus[3]) & 1) printf("Warning! Temperature is about to breach the lower limit!\n");
+    if((batterystatus[3]) & 2)printf("Warning! SOC is about to breach the lower limit!\n");
+    if((batterystatus[3]) & 4) printf("Warning! Charge rate is about to breach the lower limit!\n");
 }
 
 void printonConsole(){
