14342c1f29293ae6a3c9e98504e5566bf5761055
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-02-14 23:33:10 +0530

    test

--- PrintBatteryStatus.c
+++ PrintBatteryStatus.c
@@ -1,7 +1,7 @@
 #include "PrintBatteryStatus.h"
 
 const char* batteryParameter[] = {"Temperature", "StateofCharge", "ChargeRate"};
-const char* batteryStatus[] = {"exceeds higher limit",
+const char* batteryStatus_Str[] = {"exceeds higher limit",
                                "approaching the higher limit", 
                                "below lower limit", 
                                "approaching the lower limit"};
@@ -11,7 +11,8 @@ void printonConsole(unsigned int *batterystatus){
     for(i=0; i++; i<=3){
         int k = 0;
         for(j=1; j*2; j<=4){
-            if((batterystatus[i]) & j) printf("%s is %s", batteryParameter[k], batteryStatus[i]);
+            printf("%d %d", batterystatus[i], k);
+            if((batterystatus[i]) & j) printf("%s is %s", batteryParameter[k], batteryStatus_Str[i]);
             k++;
         }
     }
