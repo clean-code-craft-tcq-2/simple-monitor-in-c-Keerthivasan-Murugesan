881dfab9febc4f548f94399914fc2ee2e2643ae0
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-02-14 23:39:32 +0530

    Update PrintBatteryStatus.c

--- PrintBatteryStatus.c
+++ PrintBatteryStatus.c
@@ -10,7 +10,7 @@ void printonConsole(unsigned int *batterystatus){
     int i, j;
     for(i=0; i<=3; i++){
         int k = 0;
-        for(j=1; j<=4; j*2){
+        for(j=1; j<=4; j=j*2){
             printf("%d %d", batterystatus[i], k);
             if((batterystatus[i]) & j) printf("%s is %s", batteryParameter[k], batteryStatus_Str[i]);
             k++;
