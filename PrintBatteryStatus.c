72cf2cfbde673317fbaf403721f5b05c7fb1b8d2
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-02-14 23:50:00 +0530

    reduce CCN

--- PrintBatteryStatus.c
+++ PrintBatteryStatus.c
@@ -1,17 +1,21 @@
 #include "PrintBatteryStatus.h"
 
 const char* batteryParameter[] = {"Temperature", "StateofCharge", "ChargeRate"};
-const char* batteryStatus_Str[] = {"exceeds higher limit",
-                               "approaching the higher limit", 
-                               "below lower limit", 
-                               "approaching the lower limit"};
+const char* batteryStatus_Str[] = {"above higher limit",
+                                   "approaching the higher limit", 
+                                   "below lower limit", 
+                                   "approaching the lower limit"};
+
+void printalert(bool print, int parameter, int status){
+    if(print) printf("%s is %s\n", batteryParameter[parameter], batteryStatus_Str[status]);
+}
 
 void printonConsole(unsigned int *batterystatus){
     int i, j;
     for(i=0; i<=3; i++){
         int k = 0;
         for(j=1; j<=4; j=j*2){
-            if((batterystatus[i]) & j) printf("%s is %s\n", batteryParameter[k], batteryStatus_Str[i]);
+            printalert(((batterystatus[i]) & j), k, i);
             k++;
         }
     }
