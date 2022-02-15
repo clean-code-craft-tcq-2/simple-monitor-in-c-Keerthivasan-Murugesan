ca2f18ee6cf0a72f01a98e7738d94f025fa92817
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-02-15 23:46:12 +0530

    German language support

--- PrintBatteryStatus.c
+++ PrintBatteryStatus.c
@@ -3,14 +3,13 @@
 int language = GERMAN;
 
 LanguageParam alertprinter[2]= {{{"Temperature", "StateofCharge", "ChargeRate"},
-                                {"HIGH_BREACH", "HIGH_WARNING", "LOW_BREACH", "LOW_WARNING", "NORMAL"}}, 
+                                {"HIGH_BREACH", "HIGH_WARNING", "LOW_BREACH", "LOW_WARNING"}}, 
                                 {{"Temperatur", "Ladezustand", "Ladestrom"},
-                                {"HOCH_VERSTOSS", "HOCH_WARNUNG", "NIEDRIG_VERSTOSS", "NIEDRIG_WARNUNG", "NORMAL"}}};
+                                {"HOCH_VERSTOSS", "HOCH_WARNUNG", "NIEDRIG_VERSTOSS", "NIEDRIG_WARNUNG"}}};
 
 
 void printonConsole(bool print, int parameter, int status){
     if(print) printf("%s : %s\n", alertprinter[language].batteryParameter[parameter], alertprinter[language].batteryStatus_Str[status]);
-    else printf("%s : %s\n", alertprinter[language].batteryParameter[parameter], alertprinter[language].batteryStatus_Str[4]);
 }
 
 void printalert(unsigned int *batterystatus){
